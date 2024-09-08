a3c3e728c1169a69ea57d86d430873029f2e9041
IsraelRamirez1 <is326915@ucf.edu>
2024-09-08 22:52:08 +0000

    Add fracturing program

--- fracturing.c
+++ fracturing.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <math.h>  
+#include <math.h>  // to add math functions
 
 #define PI 3.14159
 
@@ -7,7 +7,7 @@
 double calculate_distance() {
     double x1, y1, x2, y2, distance;
     
-    
+    // asking input from person
     printf("Enter x1: ");
     scanf("%lf", &x1);
     printf("Enter y1: ");
@@ -17,7 +17,7 @@ double calculate_distance() {
     printf("Enter y2: ");
     scanf("%lf", &y2);
 
-    
+    // to caclulate the distance between the points
     distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     
     
@@ -26,14 +26,14 @@ double calculate_distance() {
     return distance;
 }
 
-
+// now to calculate the area of the circle
 double calculate_perimeter() {
     double distance = calculate_distance();
     double radius = distance / 2.0;
     double perimeter = 2 * PI * radius;
     
-    // Output the perimeter in the expected format
-    printf("The perimeter of the city encompassed by your request is: %.2lf\n", perimeter);
+    
+    printf("The perimeter of the city is: %.2lf\n", perimeter);
     
     return perimeter;
 }
@@ -45,17 +45,17 @@ double calculate_area() {
     double area = PI * pow(radius, 2);
     
     
-    printf("The area of the city encompassed by your request is: %.2lf\n", area);
+    printf("The area of the city is: %.2lf\n", area);
     
     return area;
 }
 
-
+// to calculate the height of the city (diameter)
 double calculate_width() {
     double distance = calculate_distance();
     
     
-    printf("The width of the city encompassed by your request is: %.2lf\n", distance);
+    printf("The width of the city is: %.2lf\n", distance);
     
     return distance;
 }
@@ -65,12 +65,12 @@ double calculate_height() {
     double distance = calculate_distance();
     
     
-    printf("The height of the city encompassed by your request is: %.2lf\n", distance);
+    printf("The height of the city is: %.2lf\n", distance);
     
     return distance;
 }
 
-
+// main function
 int main(void) {
     calculate_perimeter();
     calculate_area();
